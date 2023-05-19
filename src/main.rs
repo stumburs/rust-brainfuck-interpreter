5fd3389a28fe3d14881e1444e482db8a5a16305e
stumburs <stumburs@inbox.lv>
2023-05-19 12:04:22 +0300

    I'm struggling

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,15 @@
 // warnings, errors, all causing me night terrors
 // please, let me manage my own memory, at least a bit
 
-use std::io::stdin;
-
 const MEMORY_SIZE: usize = 30000;
 
 fn interpret(program: &str) {
-    let stdin = std::io::stdin();
-
     let mut mem: [u8; MEMORY_SIZE] = [0u8; MEMORY_SIZE];
     let mut pointer: usize = 0;
 
-    let i: usize = 0;
-    while i < program.len() {
+    let mut i: usize = 0;
+    while i < program.len() - 1 {
+        // I have no clue why it's the way it is.
         let current_char = program.as_bytes()[i];
         if current_char == b'>' {
             pointer += 1;
@@ -29,14 +26,19 @@ fn interpret(program: &str) {
             pointer -= 1;
             continue;
         }
+        // This makes it not work :)
         if current_char == b'+' {
-            mem[pointer] += 1;
+            // Can't wrap values by overflowing them, because it's Rust.
+            mem[pointer] = mem[pointer].wrapping_add(1);
             continue;
         }
+        // Don't even know about this.
         if current_char == b'-' {
-            mem[pointer] -= 1;
+            // Can't do -1, because it's unsigned and whatnot.
+            mem[pointer] = mem[pointer].wrapping_sub(1);
             continue;
         }
+        // ¯\_(ツ)_/¯
         if current_char == b'.' {
             print!("{}", mem[pointer]);
             continue;
@@ -44,6 +46,10 @@ fn interpret(program: &str) {
         if current_char == b',' {
             continue;
         }
+
+        i += 1;
+        // Funny C++ code i've yet to turn into Rust code.
+
         // case '[':
         //     if (*ptr == 0)
         //     {
